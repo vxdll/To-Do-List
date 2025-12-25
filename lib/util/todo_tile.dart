cc6f55f68af41d464d14c9fd60321ad3d5967190
GregoryV <vxdll.39@hotmail.com>
2025-12-25 16:47:32 +0200

    Complete function but no save in memory

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
-
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
     required this.onChanged,
+    required this.deleteFunction,
   });
 
   @override
@@ -18,34 +20,47 @@ class ToDoTile extends StatelessWidget {
     return Padding(
       // Внешние отступы
       padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 20.0),
-      child: Container(
-        // Отступы внитри
-        padding: EdgeInsets.all(15),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            // Checkbox
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              side: BorderSide(color: Colors.white),
-            ),
-
-            //Наименование задачи
-            Text(
-              taskName,
-              style: TextStyle(
-                color: Colors.white,
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade500,
+              borderRadius: BorderRadius.circular(10),
             ),
           ],
         ),
-        // Стилизация контейнера
-        decoration: BoxDecoration(
-          color: Colors.indigo[700],
-          borderRadius: BorderRadius.circular(10),
+        child: Container(
+          // Отступы внитри
+          padding: EdgeInsets.all(15),
+          child: Row(
+            children: [
+              // Checkbox
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                side: BorderSide(color: Colors.white),
+              ),
+
+              //Наименование задачи
+              Text(
+                taskName,
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
+          // Стилизация контейнера
+          decoration: BoxDecoration(
+            color: Colors.indigo[700],
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
       ),
     );
